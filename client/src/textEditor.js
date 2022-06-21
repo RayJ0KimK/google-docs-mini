c96d97de0818ef92d0060123232c3d1ec263d339
ricky <dehao@bu.edu>
2022-06-21 15:31:29 -0700

    <feat>: Now users inputs would only propagate if the users are in the same room

--- client/src/textEditor.js
+++ client/src/textEditor.js
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect, useState } from 'react';
 import Quill from 'quill';
 import "quill/dist/quill.snow.css";
 import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
+import { unwatchFile } from 'react-router-dom';
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,8 +18,10 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+    const { id: documentId } = useParams() //renaming id to documentID
     const [socket, setSocket] = useState()
     const [quill, setQuill] = useState() //To track the state of quill
+    console.log(documentId)
 
     //Connecting to socket
     useEffect(() => {
@@ -54,6 +58,20 @@ export default function TextEditor() {
         }
     }, [socket, quill])
 
+    //Prevent changes made to emitted by other documents
+    useEffect(() => {
+        if (socket == null || quill == null) return
+
+        //This is only executed when load-document is called
+        //What is var:document here ? 
+        socket.once("load-document", document => {
+            quill.setContents(document) //loads the document into text editor 
+            quill.enable()
+        })
+
+        socket.emit('get-document', documentId) // telling the document of which document we're part of
+    }, [socket, quill, documentId]) //What do you do with dockumentID ? 
+
     const wrapperRef = useCallback(wrapper => {
         if (wrapper == null) return
         wrapper.innerHTML = ""
@@ -63,6 +81,8 @@ export default function TextEditor() {
             theme: "snow",
             modules: { toolbar: TOOLBAR_OPTIONS },
         })
+        q.disable()
+        q.setText('Loading...')
         setQuill(q)
     }, [])
 
